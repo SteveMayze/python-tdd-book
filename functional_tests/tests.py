080cdd566f19d7b5e6e4d1cb5ba98f143cf4c053
SteveMayze <smayze@yahoo.com>
2018-12-29 17:26:18 +0100

    Uses bootstrap to improve the layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.common.exceptions import WebDriverException 
@@ -7,7 +7,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp( self ):
 		self.browser = webdriver.Firefox()
 
@@ -31,6 +31,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# Edith goes to the hom page
 		self.browser.get(self.live_server_url)
 		self.browser.set_window_size(1024, 768)
+
 
 		# She notices the input box is nicely centered
 		inputbox = self.browser.find_element_by_id('id_new_item')
