8a3d979291b7f5a70962853aef989ac1a813f9b6
SteveMayze <smayze@yahoo.com>
2019-01-21 21:33:25 +0100

    Add list owners via forms. More isolated tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,7 +10,7 @@ from lists.forms import (
 )
 from lists.models import Item, List
 from lists.views import home_page
-from lists.views import new_list2
+from lists.views import new_list
 from unittest.mock import patch, Mock
 import unittest
 
@@ -138,46 +138,27 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 		self.assertEqual(Item.objects.all().count(), 1)
 
-
-class NewListTest(TestCase):
+class NewListViewIntegratedTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
-		response = self.client.post('/lists/new', data={'text': 'A new list item'})
+		self.client.post('/lists/new', data={'text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
-	def test_redirects_after_POST(self):
-		response = self.client.post('/lists/new', data={'text': 'A new list item'})
-		new_list = List.objects.first()
-		self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-
-	def test_for_invalid_input_renderes_home_template(self):
+	def test_for_invalid_input_doesnt_save_but_shows_errors(self):
 		response = self.client.post('/lists/new', data={'text': ''})
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'home.html')
-
-	def test_validation_errors_are_shown_on_home_page(self):
-		response = self.client.post('/lists/new', data={'text': ''})
+		self.assertEqual(List.objects.count(), 0)
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-	def test_for_invalid_input_passes_form_to_template(self):
-		response = self.client.post('/lists/new', data={'text': ''})
-		self.assertIsInstance(response.context['form'], ItemForm)
-
-
-	def test_invalid_list_items_arent_saved(self):
-		self.client.post('lists/new', data={'text': ''})
-		self.assertEqual(List.objects.count(), 0)
-		self.assertEqual(Item.objects.count(), 0)		
 
 	def test_list_owner_is_saved_if_user_is_authenticated(self):
 		user = User.objects.create(email='a@b.com')
 		self.client.force_login(user)
 		self.client.post('/lists/new', data={'text': 'new item'})
 		list_ = List.objects.first()
-		self.assertEqual(list_.owner, user)		
+		self.assertEqual(list_.owner, user)
 
 
 class MyListsTest(TestCase):
@@ -191,10 +172,9 @@ class MyListsTest(TestCase):
 		correct_user = User.objects.create(email='a@b.com')
 		response = self.client.get('/lists/users/a@b.com/')
 		self.assertEqual(response.context['owner'], correct_user)
-		
 
 @patch('lists.views.NewListForm')
-class NewListViewnitTest(unittest.TestCase):
+class NewListViewUnitTest(unittest.TestCase):
 
 	def setUp(self):
 		self.request = HttpRequest()
@@ -203,13 +183,13 @@ class NewListViewnitTest(unittest.TestCase):
 
 
 	def test_passes_POST_data_NewListForm(self, mockNewListForm):
-		new_list2(self.request)
+		new_list(self.request)
 		mockNewListForm.assert_called_once_with(data=self.request.POST)
 
 	def test_saves_form_with_owner_if_form_valid(self, mockNewListForm):
 		mock_form = mockNewListForm.return_value
 		mock_form .is_valid.return_value = True
-		new_list2(self.request)
+		new_list(self.request)
 		mock_form.save.assert_called_once_with(owner=self.request.user)
 
 
@@ -220,7 +200,7 @@ class NewListViewnitTest(unittest.TestCase):
 		mock_form = mockNewListForm.return_value
 		mock_form.is_valid.return_value = True
 
-		response = new_list2(self.request)
+		response = new_list(self.request)
 
 		self.assertEqual(response, mock_redirect.return_value)
 		mock_redirect.assert_called_once_with(mock_form.save.return_value)
@@ -233,7 +213,7 @@ class NewListViewnitTest(unittest.TestCase):
 		mock_form = mockNewListForm.return_value
 		mock_form.is_valid.return_value = False
 
-		response = new_list2(self.request)
+		response = new_list(self.request)
 
 		self.assertEqual(response, mock_render.return_value)
 		mock_render.assert_called_once_with(
@@ -244,7 +224,7 @@ class NewListViewnitTest(unittest.TestCase):
 	def test_does_not_save_if_form_invalid(self, mockNewListForm):
 		mock_form = mockNewListForm.return_value
 		mock_form.is_valid.return_value = False
-		new_list2(self.request)
+		new_list(self.request)
 		self.assertFalse(mock_form.save.called)
 
 	@patch('lists.views.redirect')
@@ -254,7 +234,7 @@ class NewListViewnitTest(unittest.TestCase):
 		mock_form = mockNewListForm.return_value
 		mock_form.is_valid.return_value = True
 
-		response = new_list2(self.request)
+		response = new_list(self.request)
 
 		self.assertEqual(response, mock_redirect.return_value)
 		mock_redirect.assert_called_once_with(mock_form.save.return_value)
